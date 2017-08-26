fa7456e159969b7481aadad2913d9b718391f46b
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-08-26 17:32:51 +0530

    Added UIColorExtension + Added container view in list table view cell

--- SnapList/SNListTableViewCell.swift
+++ SnapList/SNListTableViewCell.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class SNListTableViewCell: UITableViewCell {
+    
     let titleLabel:UILabel = UILabel()
     let descriptionLabel:UILabel = UILabel()
-    
+    let containerView = UIView()
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.selectionStyle = .none
@@ -25,30 +26,44 @@ class SNListTableViewCell: UITableViewCell {
     }
     
     func createViews() {
+        self.contentView.backgroundColor = UIColor.clear
+        self.backgroundColor = UIColor.clear
+        containerView.backgroundColor = UIColor.white
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(containerView)
+        
+        
         titleLabel.textColor = UIColor.black
         titleLabel.textAlignment = .center
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(titleLabel)
+        containerView.addSubview(titleLabel)
         
         //descriptionLabel.text = "Some really long description can really come in handy here! I mean come on just look at this"
         descriptionLabel.textColor = UIColor.black
         descriptionLabel.sizeToFit()
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         descriptionLabel.numberOfLines = 0
-        contentView.addSubview(descriptionLabel)
+        containerView.addSubview(descriptionLabel)
         
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant:8),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant:-8),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant:ceil(kpadding13/2)),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -ceil(kpadding13/2)),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant:8),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant:-8),
             titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -20)
             ])
         
         NSLayoutConstraint.activate([
-            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant:8),
-            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant:-8),
-            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant:-20)
+            descriptionLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant:8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant:-8),
+            descriptionLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant:-20)
             ])
     }
 
